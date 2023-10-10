ac35394ba581f24933312b3bab935fad037f5a04
AndreSbro03 <andresbro03@gmail.com>
2023-10-10 23:32:12 +0200

    boh

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ un quantitativo corretto di memoria e non un valore predefinito.
 use rand::Rng;
 use fast_math::exp;
 
-const NUM_TEST: u32 = 50 * 1000;
-const EPS: f64 = 0.1;
-const RATE: f64 = 1.3;
-
 const INPUT: usize = 2;
 const NUM_TRAIN_SAMPLE: usize = 4;
+const NUM_TEST: u32 = 100 * 1000;
+const EPS: f64 = 0.1;
+const RATE: f64 = 1.3;
+const NUM_NEURONS: usize = 3;
+
 const AND: [[f64; 3]; NUM_TRAIN_SAMPLE] = [
     [0.0,0.0,0.0],
     [0.0,1.0,0.0],
@@ -43,37 +44,29 @@ const NAND: [[f64; 3]; NUM_TRAIN_SAMPLE] = [
 const DATA: [[f64; 3]; NUM_TRAIN_SAMPLE] = XOR;
 const LEN_ARC: usize = 2;
 const ARC: [usize; LEN_ARC] = [2,1];
-const NUM_NEURONS: usize = 3;
 
 #[derive(Debug)]
 #[derive(Default)]
 struct Neuron {
-    w: Vec<f64>,
+    w: [f64; INPUT],
     b: f64,
 }
 
 fn main(){
 
     let mut nrs: [Neuron; NUM_NEURONS] = Default::default();
-    let mut cont = 0;
-    for layout_idx in 0..LEN_ARC{
-        for _ in 0..ARC[layout_idx]{
-            nrs[cont] = get_randomize_neuron(layout_idx);
-            cont += 1;
-        }   
-    }
-
-    println!("{:#?}", nrs);
-    
+    for i in 0..NUM_NEURONS{
+        nrs[i] = getRandomizedNeuron();
+    }   
 
     for _ in 0..NUM_TEST {
-            update_weights(&mut nrs, 1);
+            updateWeights(&mut nrs, 1);
         }   
      
     
     for i in 0..=1{
         for k in 0..=1{
-            print_results(&mut nrs, [i as f64,k as f64]);
+            printResults(&mut nrs, [i as f64,k as f64]);
         }
     }
 
@@ -98,43 +91,46 @@ fn sigmuid(x: f64) -> f64 {
 }
 
 fn randomf() -> f64 {
-    (rand::thread_rng().gen_range(0..=1000) as f64) / 1000.0
+    (rand::thread_rng().gen_range(-1000..=1000) as f64) / 1000.0
 }
 
-fn cost(nrs: &mut [Neuron; NUM_NEURONS]) -> f64 {
-    let mut out: f64 = -1.0;
+fn cost(Neurons: &mut [Neuron; NUM_NEURONS]) -> f64 {
+    let mut out: f64 = 0.0;
     let mut res: f64 = -1.0;
 
-    for sample_idx in 0..NUM_TRAIN_SAMPLE{ //Per ogni tupla d'input
-        let mut neuron_idx: usize = 0;
-        let mut output: Vec<f64> = Vec::with_capacity(ARC[0]);
+    for a in 0..NUM_TRAIN_SAMPLE{ //Per ogni tupla d'input
 
-        for layer_idx in 0..LEN_ARC { //Per ogni Layer dell'architettura
+        let mut output: Vec<f64> = Vec::with_capacity(ARC[0]);
+        let mut idx: usize = 0;
+
+        for b in 0..LEN_ARC { //Per ogni Layer dell'architettura
             
             let mut input: Vec<f64> = Vec::with_capacity(output.len());
             input = output.clone();
-            if layer_idx != 0 {
-                let mut output: Vec<f64> = Vec::with_capacity(ARC[layer_idx]);
+            if b != 0 {
+                let mut output: Vec<f64> = Vec::with_capacity(ARC[b]);
             }
 
-            for _ in 0..(ARC[layer_idx]){ //Per ogni Neurone del Layer                
-                let mut somma = 0.0;                
-                for inp_idx in 0..((nrs[neuron_idx].w).len()){ //Per ogni input
-                    //println!("{} {} {} {}", idx, d, nrs.len(), nrs[0].w.len());
-                    if layer_idx == 0{
-                        somma += nrs[neuron_idx].w[inp_idx] * DATA[sample_idx][inp_idx];
+            for c in 0..(ARC[b]){ //Per ogni Neurone del Layer                
+                
+                let mut inp = 0.0;
+                for d in 0..INPUT{ //Per ogni input
+                    //println!("{} {} {} {} {}", idx, a, b, c, d);
+                    if b == 0{
+                        inp += Neurons[idx].w[d] * DATA[a][d];
                     }
                     else {
-                        somma += nrs[neuron_idx].w[inp_idx] * input[inp_idx];
+                        inp += Neurons[idx].w[d] * input[d];
                     }
                 }
-                somma += nrs[neuron_idx].b;
-                output.push(sigmuid(somma));
-                neuron_idx += 1;
+                
+                inp += Neurons[idx].b;
+                output.push(sigmuid(inp));
+                idx += 1;
             }
             res = output[0];
         }
-        let dst:    f64  = res - DATA[sample_idx][INPUT];         
+        let dst:    f64  = res - DATA[a][INPUT];         
         out += dst * dst;
     }
 
@@ -142,40 +138,30 @@ fn cost(nrs: &mut [Neuron; NUM_NEURONS]) -> f64 {
 
 }
 
-fn get_randomize_neuron(lidx: usize) -> Neuron {
-    let mut n: Neuron = Neuron{ w: Vec::new(), b: 0.0};
-    if lidx == 0 {
-        n.w = Vec::with_capacity(INPUT);
-        for _ in 0..INPUT {
-            (n.w).push(randomf()); 
-        }    
+fn getRandomizedNeuron() -> Neuron {
+    let mut n: Neuron = Neuron{ w: [0.0 , 0.0], b: 0.0};
+    for i in 0..INPUT {
+        n.w[i] = randomf(); 
     }
-    else {
-        n.w = Vec::with_capacity(ARC[(lidx - 1) as usize]);
-        for _ in 0..ARC[(lidx - 1) as usize] {
-            (n.w).push(randomf());
-        }   
-    }   
-
     n.b = randomf();
     n
 }
 
-fn update_weights(nrs: &mut [Neuron; NUM_NEURONS], pr: u8) {
+fn updateWeights(nrs: &mut [Neuron; NUM_NEURONS], pr: u8) {
     let cst: f64 =  cost(nrs); 
 
     let mut idx: usize = 0;
 
-    for layer_idx in 0..LEN_ARC { //Per ogni Layer dell'architettura
-            for _ in 0..ARC[layer_idx]{ //Per ogni Neurone del Layer 
-                let weight: Vec<f64> = (nrs[idx].w).clone();    
-                let bias: f64 = nrs[idx].b;
-                for inp_idx in 0..((nrs[idx].w).len()){ //Per ogni input
+    for a in 0..LEN_ARC { //Per ogni Layer dell'architettura
+            for _ in 0..ARC[a]{ //Per ogni Neurone del Layer 
+                let weight = nrs[idx].w;    
+                let bias = nrs[idx].b;
+                for c in 0..INPUT{ //Per ogni input
                     
-                    nrs[idx].w[inp_idx] += EPS;
+                    nrs[idx].w[c] += EPS;
                     let dw: f64 = (cost(nrs) - cst) / EPS; 
-                    nrs[idx].w[inp_idx] = weight[inp_idx];
-                    nrs[idx].w[inp_idx] -= dw * RATE;
+                    nrs[idx].w[c] = weight[c];
+                    nrs[idx].w[c] -= dw * RATE;
                 }
 
                 nrs[idx].b += EPS;
@@ -190,48 +176,33 @@ fn update_weights(nrs: &mut [Neuron; NUM_NEURONS], pr: u8) {
         println!("-----------------------");
         for i in 0..=1{
             for k in 0..=1{
-                print_results(nrs, [i as f64,k as f64]);
+                printResults(nrs, [i as f64,k as f64]);
             }
         }
         println!("------------------------");
     }
 }
 
-fn print_results(nrs: &mut [Neuron; NUM_NEURONS], data: [f64; INPUT]){
+fn printResults(nrs: &mut [Neuron; NUM_NEURONS], data: [f64; INPUT]){
     let mut idx: usize = 0;
-    let mut res: f64 = -1.0;
-    let mut output: Vec<f64> = Vec::with_capacity(ARC[0]);
+    let mut y: [f64; INPUT] = [0.0, 0.0];
     
     for b in 0..LEN_ARC { //Per ogni Layer dell'architettura
-        /*
-        In questa parte andiamo a copiare il risultato del output che
-        diventerà l'input per la prossima serie di neuroni. In più se 
-        non siamo alla prima simulazione il vettore output verrà 
-        reinizializzato
-        */
-        let mut input: Vec<f64> = Vec::with_capacity(output.len());
-        input = output.clone();
-        if b != 0 {
-            let mut output: Vec<f64> = Vec::with_capacity(ARC[b]);
-        }
-
-        for _ in 0..(ARC[b]){ //Per ogni Neurone del Layer                
-            let mut somma = 0.0;                
-            for d in 0..((nrs[idx].w).len()){ //Per ogni input
-                //println!("{} {} {} {} {}", idx, a, b, c, d);
-                if b == 0{
-                    somma += nrs[idx].w[d] * data[d];
+        for c in 0..ARC[b]{ //Per ogni Neurone del Layer                
+                let mut inp = 0.0;
+                for d in 0..INPUT{ //Per ogni input
+                    if b == 0{
+                        inp += nrs[idx].w[d] * data[d];
+                    }
+                    else {
+                        inp += nrs[idx].w[d] * y[d];
+                    }
                 }
-                else {
-                    somma += nrs[idx].w[d] * input[d];
-                }
+                inp += nrs[idx].b;
+                y[c] = sigmuid(inp);
+                idx += 1;
             }
-            somma += nrs[idx].b;
-            output.push(sigmuid(somma));
-            idx += 1;
-        }
-        res = output[0];
     }
 
-    println!("{} {} {}", data[0], data[1], res);
+    println!("{} {} {}", data[0], data[1], y[0])
 }
